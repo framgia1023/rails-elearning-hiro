d21b335cec2903ca8b3cd820959a6ff15090ec6d
上原大尚 <ueharadainao@MacBook-Pro-Ue.local>
2018-12-06 13:30:55 +0800

    Create signuppage,function

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  validates :name, presence:true, length: { maximum:25, minimum:2 }
+
+  before_save { email.downcase! }
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum:50 },
+                                   format: { with: EMAIL_REGEX },
+                                   uniqueness: { case_sensitive:false }
+  has_secure_password
+  validates :password, length: { minimum:6 }, allow_nil: true
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
 root 'home_pages#home'
+resources :users
 end
