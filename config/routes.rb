da693ee5e447395220f9fa8eee3b029d68945eed
上原大尚 <ueharadainao@MacBook-Pro-Ue.local>
2018-12-06 16:06:01 +0800

    Create loginlogoutpage_, function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-root 'home_pages#home'
-resources :users
+    root 'home_pages#home'
+    resources :users
+    get '/login', to: 'sessions#new'
+    delete '/logout', to: 'sessions#destroy'
+    resources :sessions, only: [:create]
 end
