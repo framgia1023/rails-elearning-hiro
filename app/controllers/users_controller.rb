2c83ddd65efed19203f4f005e7b5b68a732130e5
上原大尚 <ueharadainao@MacBook-Pro-Ue.local>
2018-12-06 14:59:15 +0800

    remove spaces, add error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,15 +13,9 @@ class UsersController < ApplicationController
         end
     end
 
-
-
-
-
     private
     def user_params
         params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
-
-
-
+    
 end
