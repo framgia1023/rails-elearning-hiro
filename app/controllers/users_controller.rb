b05a24482d197afdc9129461441cf61575b23662
上原大尚 <ueharadainao@MacBook-Pro-Ue.local>
2018-12-07 17:40:33 +0800

    Modify linkurl and paginate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     end
 
     def index
-        @users = User.paginate(page: params[:page], per_page: 10)
+        @users = User.paginate(page: params[:page], per_page: 4)
     end
 
     private
