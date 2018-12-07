97377590f4b3b4b550f8ddba56516afe3796dc05
上原大尚 <ueharadainao@MacBook-Pro-Ue.local>
2018-12-07 23:04:40 +0800

    Create dashboardpage and count function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,13 +18,31 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def feed
+  end
+
   def index
     @users = User.paginate(page: params[:page], per_page: 4)
   end
+
+  def following
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
+  end
+
+
 
   private
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 	root 'home_pages#home'
 	resources :users
+	get '/feed', to: 'users#feed'
 	get '/login', to: 'sessions#new'
 	delete '/logout', to: 'sessions#destroy'
 	resources :sessions, only: [:create]
 	resources :relationships, only: [:create, :destroy]
+	resources :users do
+    	member do
+    		get :following, :followers
+		end
+    end
 end
