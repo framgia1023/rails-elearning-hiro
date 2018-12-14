19b1870871f45eb2986c6ae8f84abe7ec126ff73
Hironao Uehara <ueueue0217@gmail.com>
2018-12-14 17:25:33 +0800

    modify some code about categories_index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,11 +3,9 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.paginate(page: params[:page], per_page: 4)
     @words = Word.all
-    @wordsarray = @words.to_a
-    @array = []
+    @id_array = []
     @words.each do |word|
-      @array << word.category_id
+      @id_array << word.category_id
     end
-    @array2 = @array.uniq
   end
 end
