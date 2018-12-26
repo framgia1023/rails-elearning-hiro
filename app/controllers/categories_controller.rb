f0b8c8886936d98eeec89db6186083f4bc329407
Hironao Uehara <ueueue0217@gmail.com>
2018-12-26 16:43:15 +0800

    modify categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,15 @@
 class CategoriesController < ApplicationController
 
   def index
-    @user = User.find(current_user.id)
     @lesson = Lesson.new
     if params[:status] == "learned"
-      @categories = Category.joins(:words).group(:id).joins(:lessons).where(lessons: {user_id: @user.id}).where.not(lessons: {results: nil}).paginate(page: params[:page], per_page: 4)
+      @categories = Category.joins(:lessons).where(lessons: {user_id: current_user.id}).where.not(lessons: {results: nil})
     elsif params[:status] == "unlearned"
-      @categories = Category.joins(:words).group(:id).joins("LEFT OUTER JOIN lessons ON categories.id = lessons.category_id AND lessons.user_id = #{@user.id}").where(lessons: {results: nil}).paginate(page: params[:page], per_page: 4)
-      #left_joins(:lessons).where(lessons: {results: nil}).paginate(page: params[:page], per_page: 4)
+      @categories = Category.joins("LEFT OUTER JOIN lessons ON categories.id = lessons.category_id AND lessons.user_id = #{current_user.id}").where(lessons: {results: nil})
     else
-      @categories = Category.joins(:words).group(:id).paginate(page: params[:page], per_page: 4) #.where.not(words: {category_id: nil})
+      @categories = Category.all 
     end
+    @categories = @categories.joins(:words).group(:id).paginate(page: params[:page], per_page: 4)
   end
   
 end
