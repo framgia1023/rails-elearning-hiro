ae7c80753d276d314187141fadab20caa18abb67
hironaooo <43838050+hironaooo@users.noreply.github.com>
2018-12-28 17:13:02 +0800

    Add design and create category filter function (#19)
    * Add design and create category filter function
    
    * move form from table

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,8 +1,15 @@
 class WordsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @lessons = @user.lessons
-    @learnwordscount =  Lesson.joins("INNER JOIN words ON lessons.category_id = words.category_id").where(lessons: {user_id: @user.id}).count
+    @categories = @user.categories
+    if params[:title].present?
+      @category = Category.find(params[:title])
+      @answers = @category.answers.all.where(lessons: {user_id: @user.id})
+    else
+      @answers = @user.answers
+    end
+    @answers = @answers.paginate(page: params[:page], per_page: 7)
+    @learnwordscount =  Lesson.joins("INNER JOIN words ON lessons.category_id = words.category_id").where(lessons: {user_id: @user.id}).where.not(lessons: {results: nil}).count
   end
   
 end
